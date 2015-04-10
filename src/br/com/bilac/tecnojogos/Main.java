7b53146f4f58e2e006d03710c3f8d2b3a90f4167
Mateus Souza da Silva <mma.teuzzz@gmail.com>
2015-04-10 12:44:05 -0300

    Acréscimo de código na classe Main.

--- src/br/com/bilac/tecnojogos/Main.java
+++ src/br/com/bilac/tecnojogos/Main.java
@@ -1,8 +1,20 @@
 package br.com.bilac.tecnojogos;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Arena arena = new Arena(600, 400);
+        arena.adicionaTanque(new Tanque(100, 200, 0, Color.BLUE));
+        arena.adicionaTanque(new Tanque(200, 200, 45, Color.RED));
+        arena.adicionaTanque(new Tanque(470, 360, 90, Color.GREEN));
+        arena.adicionaTanque(new Tanque(450, 50, 157, Color.YELLOW));
+        JFrame f = new JFrame("Tanques");
+        f.getContentPane().add(arena);
+        f.pack();
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
